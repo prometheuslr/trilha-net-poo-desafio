eddf97190dc3e1f23d247388950d110fa8dd2e6d
prometheuslr <prometheus.inves@gmail.com>
2023-12-23 00:51:03 -0300

    final

--- Program.cs
+++ Program.cs
@@ -2,6 +2,15 @@
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
 
-Nokia nk1 = new Nokia("11111", "modelo", "1111", 12);
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456", "Modelo 10", "222222222", 120);
 
-nk1.Ligar();
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("6565656", "Modelo 24", "4444444", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("YouTube");
